3a1188d94324026e9a67d4cd68f16d883598d962
Jayampathy Balasuriya <j2damax@gmail.com>
2025-12-03 12:21:58 +0530

    clean project

--- task2/scripts/download-dataset.py
+++ task2/scripts/download-dataset.py
@@ -6,15 +6,10 @@
 import json
 from sodapy import Socrata
 
-# Unauthenticated client only works with public data sets. Note 'None'
-# in place of application token, and no username or password:
 client = Socrata("data.austintexas.gov", None)
 
-# First 2000 results, returned as JSON from API / converted to Python list of
-# dictionaries by sodapy.
-# First 2000 results, returned as JSON from API / converted to Python list of
-# dictionaries by sodapy.
-results = client.get("sh59-i6y9", limit=100000, order="read_date DESC")
+# retrieve up to 1 million records, ordered by most recent read_date
+results = client.get("sh59-i6y9", limit=1000000, order="read_date DESC")
 
 # Print first few records (equivalent to head())
 print("First 5 records:")
